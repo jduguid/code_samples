f28efb33167e005bda1247e9b9a3434a2de320a9
jduguid <james.duguid1@gmail.com>
2024-04-03 12:48:23 +0200

    Temporarily switched from pyproject.toml to setup.py only workflow because I couldn't pip install -e . to work on package in editable mode. This was a known bug in pip that was supposedly fixed, but doesn't appear to be fixed in my case.

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="outlier_detection",
+    version="0.1",
+    description="Toy examples of outlier detection routines for use as code samples.",
+    packages=find_packages(),
+    url="https://github.com/jduguid/code_samples",
+    author="James Duguid",
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "scipy",
+    ]
+)
